dbdc71040bbb5e1a1a154c67a9c6be1828ee14e6
shivanshu nigam <shivanshuN@200oksolutions.com>
2025-10-20 11:47:37 +0530

    asd

--- routes/landingLeadRoutes.js
+++ routes/landingLeadRoutes.js
@@ -7,7 +7,7 @@ const {
 
 const router = express.Router();
 
-router.post("/", createLandingLead); // POST https://api.zentroverse.com/api/landing-leads
+router.post("/createLandingLead", createLandingLead); // POST https://api.zentroverse.com/api/landing-leads
 router.get("/", listLandingLeads); // (optional) list for admin/testing
 
 module.exports = router;
